ae27220df7904a90a6244965e95385b524fae9bd
TranMinhTamttttt <tamtran11037469@gmail.com>
2025-10-01 13:07:23 +0700

    Revert "Merge branch 'LongVB' into Taam"
    This reverts commit 4c7c5f4b10d5ead3ce713e870d03067cd5ef5a12, reversing
    changes made to 62453defe382f1da02efb14acbd781bf60f40318.

--- AttendanceSystemProject/Models/Department.cs
+++ AttendanceSystemProject/Models/Department.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -6,26 +5,33 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AttendanceSystemProject.Models
 {
-    [Table("Departments")] // map ?úng tên b?ng trong SQL
     public class Department
     {
         [Key]
         public int DepartmentId { get; set; }
 
-        [Required, StringLength(100)]
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
 
-        [Required, StringLength(20)]
-        public string Code { get; set; } // s? ki?m tra unique ? controller (an toàn)
+        [StringLength(20)]
+        public string Code { get; set; }
 
         [StringLength(500)]
         public string Description { get; set; }
 
         public bool IsActive { get; set; } = true;
 
-        // Navigation (n?u b?n dùng)
         public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        // Navigation properties
         public virtual ICollection<User> Users { get; set; }
         public virtual ICollection<Class> Classes { get; set; }
+
+        public Department()
+        {
+            Users = new HashSet<User>();
+            Classes = new HashSet<Class>();
+        }
     }
 }
